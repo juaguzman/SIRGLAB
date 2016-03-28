9a4c646a10b1d91a21c6fcc9d2d949a6ace37275
juan <juan@Juank-PC>
2016-03-27 20:12:37 -0500

    digrama y horario fin

--- includes/accionesMN.php
+++ includes/accionesMN.php
@@ -55,7 +55,7 @@ static function ListarMonitores()
       $h1="07:00:00";
       $h2="08:00:00";
       $h3="09:00:00";
-      $h4="010:00:00";
+      $h4="10:00:00";
       $h5="11:00:00";
       $h6="12:00:00";
       $h7="13:00:00";
@@ -68,7 +68,7 @@ static function ListarMonitores()
         include 'db_connect.php';
         $consulta= "SELECT dia, DATE_FORMAT(horaentra , '%T') as horaentra, DATE_FORMAT(horasale , '%T') as horasale FROM horarios WHERE monitores_cedula = $cedu";
         $result   = $mysqli->query($consulta);
-        
+        echo "$result->num_rows";
          echo "<table id=res border= 3px solid  > \n";
          echo "<tr> <td colspan=9 id=titu >Horario de asistencia</td> </tr> "
          . "<tr><td>HORAS</td><td>LUNES</td><td>MARTES</td><td>MIERCOLES</td><td>JUEVES</td><td>VIERNES</td></tr>";
@@ -151,6 +151,146 @@ static function ListarMonitores()
             if(isset($evie,$svie))
             {if($evie<=$h4 && $svie>=$h5){ echo "<td id=ok></td></tr>"; } else { echo "<td></td></tr>";}}else { echo "<td></td></tr>";}
             
+            
+             echo "<tr><td>11:00 AM</td>";
+            /*Comparacion para horas de el lunes*/
+            if(isset($elun,$slun))
+            {if($elun<=$h5 && $slun>=$h6){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Martes*/
+            if(isset($emar,$smar))
+            {if($emar<=$h5 && $smar>=$h6){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Miercoles*/
+            if(isset($emier,$smier))
+            {if($emier<=$h5 && $smier>=$h6){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Jueves*/
+            if(isset($ejue,$sjue))
+            {if($ejue<=$h5 && $sjue>=$h6){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Viernes*/
+            if(isset($evie,$svie))
+            {if($evie<=$h5 && $svie>=$h6){ echo "<td id=ok></td></tr>"; } else { echo "<td></td></tr>";}}else { echo "<td></td></tr>";}
+            
+            
+            echo "<tr><td>12:00 PM</td>";
+            /*Comparacion para horas de el lunes*/
+            if(isset($elun,$slun))
+            {if($elun<=$h6 && $slun>=$h7){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Martes*/
+            if(isset($emar,$smar))
+            {if($emar<=$h6 && $smar>=$h7){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Miercoles*/
+            if(isset($emier,$smier))
+            {if($emier<=$h6 && $smier>=$h7){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Jueves*/
+            if(isset($ejue,$sjue))
+            {if($ejue<=$h6 && $sjue>=$h7){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Viernes*/
+            if(isset($evie,$svie))
+            {if($evie<=$h6 && $svie>=$h7){ echo "<td id=ok></td></tr>"; } else { echo "<td></td></tr>";}}else { echo "<td></td></tr>";}
+            
+            
+            echo "<tr><td>1:00 PM</td>";
+            /*Comparacion para horas de el lunes*/
+            if(isset($elun,$slun))
+            {if($elun<=$h7 && $slun>=$h8){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Martes*/
+            if(isset($emar,$smar))
+            {if($emar<=$h7 && $smar>=$h8){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Miercoles*/
+            if(isset($emier,$smier))
+            {if($emier<=$h7 && $smier>=$h8){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Jueves*/
+            if(isset($ejue,$sjue))
+            {if($ejue<=$h7 && $sjue>=$h8){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Viernes*/
+            if(isset($evie,$svie))
+            {if($evie<=$h7 && $svie>=$h8){ echo "<td id=ok></td></tr>"; } else { echo "<td></td></tr>";}}else { echo "<td></td></tr>";}
+            
+            
+             echo "<tr><td>2:00 PM</td>";
+            /*Comparacion para horas de el lunes*/
+            if(isset($elun,$slun))
+            {if($elun<=$h8 && $slun>=$h9){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Martes*/
+            if(isset($emar,$smar))
+            {if($emar<=$h8 && $smar>=$h9){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Miercoles*/
+            if(isset($emier,$smier))
+            {if($emier<=$h8 && $smier>=$h9){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Jueves*/
+            if(isset($ejue,$sjue))
+            {if($ejue<=$h8 && $sjue>=$h9){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Viernes*/
+            if(isset($evie,$svie))
+            {if($evie<=$h8 && $svie>=$h9){ echo "<td id=ok></td></tr>"; } else { echo "<td></td></tr>";}}else { echo "<td></td></tr>";}
+            
+            echo "<tr><td>3:00 PM</td>";
+            /*Comparacion para horas de el lunes*/
+            if(isset($elun,$slun))
+            {if($elun<=$h9 && $slun>=$h10){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Martes*/
+            if(isset($emar,$smar))
+            {if($emar<=$h9 && $smar>=$h10){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Miercoles*/
+            if(isset($emier,$smier))
+            {if($emier<=$h9 && $smier>=$h10){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Jueves*/
+            if(isset($ejue,$sjue))
+            {if($ejue<=$h9 && $sjue>=$h10){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Viernes*/
+            if(isset($evie,$svie))
+            {if($evie<=$h9 && $svie>=$h10){ echo "<td id=ok></td></tr>"; } else { echo "<td></td></tr>";}}else { echo "<td></td></tr>";}
+            
+            
+            echo "<tr><td>4:00 PM</td>";
+            /*Comparacion para horas de el lunes*/
+            if(isset($elun,$slun))
+            {if($elun<=$h10 && $slun>=$h11){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Martes*/
+            if(isset($emar,$smar))
+            {if($emar<=$h10 && $smar>=$h11){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Miercoles*/
+            if(isset($emier,$smier))
+            {if($emier<=$h10 && $smier>=$h11){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Jueves*/
+            if(isset($ejue,$sjue))
+            {if($ejue<=$h10 && $sjue>=$h11){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Viernes*/
+            if(isset($evie,$svie))
+            {if($evie<=$h10 && $svie>=$h11){ echo "<td id=ok></td></tr>"; } else { echo "<td></td></tr>";}}else { echo "<td></td></tr>";}
+            
+            echo "<tr><td>5:00 PM</td>";
+            /*Comparacion para horas de el lunes*/
+            if(isset($elun,$slun))
+            {if($elun<=$h11 && $slun>=$h12){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Martes*/
+            if(isset($emar,$smar))
+            {if($emar<=$h11 && $smar>=$h12){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Miercoles*/
+            if(isset($emier,$smier))
+            {if($emier<=$h11 && $smier>=$h12){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Jueves*/
+            if(isset($ejue,$sjue))
+            {if($ejue<=$h11 && $sjue>=$h12){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Viernes*/
+            if(isset($evie,$svie))
+            {if($evie<=$h11 && $svie>=$h12){ echo "<td id=ok></td></tr>"; } else { echo "<td></td></tr>";}}else { echo "<td></td></tr>";}
+            
+            echo "<tr><td>6:00 PM</td>";
+            /*Comparacion para horas de el lunes*/
+            if(isset($elun,$slun))
+            {if($slun==$h12){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Martes*/
+            if(isset($emar,$smar))
+            {if( $smar==$h12){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Miercoles*/
+            if(isset($emier,$smier))
+            {if($smier==$h12){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Jueves*/
+            if(isset($ejue,$sjue))
+            {if($sjue==$h12){ echo "<td id=ok></td>"; } else { echo "<td></td>";}}else { echo "<td></td>";}
+            /*Comparacion para horas de el Viernes*/
+            if(isset($evie,$svie))
+            {if($svie==$h12){ echo "<td id=ok></td></tr>"; } else { echo "<td></td></tr>";}}else { echo "<td></td></tr>";}
             
             
   }
